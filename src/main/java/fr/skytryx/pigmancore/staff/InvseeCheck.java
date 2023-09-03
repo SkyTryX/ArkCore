937f828311a4c5f3274738eef5f9187f27969857
SkyTryX <dorian.gil34@gmail.com>
2023-09-03 16:18:15 +0200

    - Modified join/quit msgs

--- src/main/java/fr/skytryx/pigmancore/staff/InvseeCheck.java
+++ src/main/java/fr/skytryx/pigmancore/staff/InvseeCheck.java
@@ -1,13 +1,27 @@
 package fr.skytryx.pigmancore.staff;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class InvseeCheck implements Listener {
 
     @EventHandler
     public void InvseeUnDupe(InventoryClickEvent event) {
         if (event.getView().getTitle().equals("§7Invsee")) event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+        event.joinMessage(Component.text( "[+]"+event.getPlayer().getName()).color(TextColor.color(0, 255, 0)));
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event){
+        event.quitMessage(Component.text( "[-]"+event.getPlayer().getName()).color(TextColor.color(255, 0, 0)));
     }
 }
